dd7c157c7f9d489e734a9e12e9fa89556acadb58
AkshayXD27 <lostamongstars27@gmail.com>
2026-09-07 21:21:33 +0530

    todolist

---
+++ Js-basics/todo.js
@@ -0,0 +1,33 @@
+const tasks = [];
+const time = [];
+function render() {
+  let taskHolder = "";
+
+  for (let i = 0; i < tasks.length; i++){
+    const html = ` <p> ${tasks[i]} <span> ${time[i]} </span>  <span> <button onclick="deleteTask(${i})"> Delete </button> </span></p> `;
+    taskHolder += html;
+     }
+  document.querySelector('.tasks').innerHTML = taskHolder;
+}
+
+
+function taskAdd() {
+  const item = document.querySelector('.userTask').value;
+  const item2 = document.querySelector('.time').value;
+  console.log(item,item2);
+  tasks.push(item);
+  time.push(item2);
+  console.log(tasks,time);
+  render();
+}
+
+function deleteTask(i) {
+  console.log(i);
+  if (i != undefined && i >= 0 && i!=null) {
+    console.log(i);
+    tasks.pop(i);
+    time.pop(i);
+    console.log(tasks,time);
+    render();
+  }
+}
